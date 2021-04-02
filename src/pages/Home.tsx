21c374a82e74a618fdaaaea098fac9033aa9d02f
SaadShakib <imsaadshakib@gmail.com>
2021-04-02 17:56:12 +0600

    fine

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { RouteComponentProps } from '@reach/router'; 
-import Chart from 'react-apexcharts'   
+import { RouteComponentProps } from '@reach/router';  
 
 
 
